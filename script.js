dc926c23d4d375387172b386165bee39c3512c6e
oyi2000 <142994976+oyi2000@users.noreply.github.com>
2025-03-04 18:15:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-// Efecto de scroll suave para el menú
+// Scroll suave para los enlaces del menú y botón
 document.querySelectorAll('nav a, .btn').forEach(link => {
-  link.addEventListener('click', function (e) {
-    // Si el enlace contiene un hash (referencia a id)
-    if(this.hash !== "") {
+  link.addEventListener('click', function(e) {
+    if (this.hash !== "") {
       e.preventDefault();
       const targetId = this.hash.substring(1);
       const target = document.getElementById(targetId);
-      
       window.scrollTo({
-        top: target.offsetTop - 70,
+        top: target.offsetTop - 80,
         behavior: "smooth"
       });
     }
