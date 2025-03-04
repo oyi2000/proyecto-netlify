8d5b6d4cf36f74ad1b8fcc3e89d0a3597ec94391
oyi2000 <142994976+oyi2000@users.noreply.github.com>
2025-03-04 18:11:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// Puedes agregar interacciones en JavaScript si lo deseas
-// Este código es para agregar animación de desplazamiento suave cuando se hace clic en los enlaces del menú
-
-document.querySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    
-    const targetId = this.getAttribute('href').substring(1);
-    const targetElement = document.getElementById(targetId);
-    
-    window.scrollTo({
-      top: targetElement.offsetTop - 80, // Ajustamos para que la cabecera no tape el contenido
-      behavior: 'smooth'
-    });
+// Efecto de scroll suave para el menú
+document.querySelectorAll('nav a, .btn').forEach(link => {
+  link.addEventListener('click', function (e) {
+    // Si el enlace contiene un hash (referencia a id)
+    if(this.hash !== "") {
+      e.preventDefault();
+      const targetId = this.hash.substring(1);
+      const target = document.getElementById(targetId);
+      
+      window.scrollTo({
+        top: target.offsetTop - 70,
+        behavior: "smooth"
+      });
+    }
   });
 });
